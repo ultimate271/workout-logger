5c0e1dfda5de088abfa4c96bb110270668b9b4e3
Brett Webster <bfireflash@gmail.com>
2017-03-23 17:46:13 -0500

    Started some pretty empty files for the serializor, still need to delete a lot of obsolete shit in the model.

---
+++ WorkoutLogger/Model/XmlSerialization/XmlSerializor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Xml.Linq;
+using System.Linq;
+
+namespace XmlSerializor{
+	public static class XmlSerializorExtensions{
+		#region MeaninglessExtensions
+		public static XElement SerializeToXml(this object o){ return o.SerializeToXml("Root"); }
+		#endregion
+		public static XElement SerializeToXml(this object o, string rootName){
+			return null;
+		}
+		
+		public static object DeserializeFromXml(this XElement x){
+			return null;
+		}
+
+	}
+}
---
+++ WorkoutLogger/Model/XmlSerialization/XmlSerializorDeclarations.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace XmlSerializor{
+	
+	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+	public class XmlSerializableAttribute : Attribute{ }
+}
